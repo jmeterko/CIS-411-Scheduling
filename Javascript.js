b926bb8789f5c63804268c0604af433dd77a8283
stevens444 <s.g.smathers@eagle.clarion.edu>
2018-09-17 15:13:11 -0400

    "And" button functionality
    Clicking the "And" button on the New Student Question page now dynamically makes visible the divs as you go down the page.

--- Javascript.js
+++ Javascript.js
@@ -1,4 +1,8 @@
-var $=document.getElementById();
+//GLOBALS
+var and=1;
+var or=1;
+
+//var document.getElementById=document.getElementById();
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
@@ -21,14 +25,15 @@ window.onclick = function(event) {
     }
 }
 
-function makeDivVisible(){
-    var and=1;
-    var or=1;
+function makeDivVisibleAnd(){
+    document.getElementById("divAnd" + and).removeAttribute("class","hiddenDiv");
+    document.getElementById("divAnd" + and).setAttribute("class","visibleDiv");
+    and++;
     //alert("divAnd"+and);
-    document.getElementById("divAnd1").removeAttribute("class","hiddenDiv");
-    document.getElementById("divAnd1").setAttribute("class","visibleDiv");
 }
 
-$('.message a').click(function(){
-    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-});
+function makeDivInvisible(){
+    and--;
+    document.getElementById("divAnd" + and).removeAttribute("class","visibleDiv");
+    document.getElementById("divAnd" + and).setAttribute("class","hiddenDiv");
+}
