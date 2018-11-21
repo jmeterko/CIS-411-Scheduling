e5c52603212a35db0a57377174858a14e83a89c6
dmodonnell <36867748+dmodonnell@users.noreply.github.com>
2018-11-21 18:37:22 -0500

    Security Attempt 1
    Committing so I can access Jerad's branch

--- controller/controller.php
+++ controller/controller.php
@@ -1,27 +1,27 @@
 <?php
 session_start();
-$_SESSION['username'] = "admin";  //jeradstuff, comment or delete this later
+/*$_SESSION['username'] = "admin";  //jeradstuff, comment or delete this later*/
 require_once("../security/model.php");
 require_once("../model/StudentQuestion.php");
 require_once '../model/model.php';//require the functions from the model.php file
 unQuote();          //make sure that magic_quotes_gpc added slashes are stripped back out if they are enabled.
-/*if (isset($_POST['action'])) {  // check get and post
+if (isset($_POST['action'])) {  // check get and post
     $action = $_POST['action'];
 } else if (isset($_GET['action'])) {
     $action = $_GET['action'];
 } else {
     include('../view/index.php');  // default action
     exit();
-}*/
+}
 
 
-/*if (!userIsAuthorized($action)) {
+if (!userIsAuthorized($action)) {
     if(!loggedIn()) {
         header("Location:../security/index.php?action=SecurityLogin&RequestedPage=" . urlencode($_SERVER['REQUEST_URI']));
     } else {
         include('../security/not_authorized.html');
     }
-} else */    /*{*/
+} else {
 
     if (isset($_POST['action'])) {  // check get and post
         $action = $_POST['action'];
@@ -96,7 +96,7 @@ unQuote();          //make sure that magic_quotes_gpc added slashes are stripped
         default:
             include('../view/LoginPage.php');   // default
     }
-/*}*/
+}
 
 function unQuote() {//strips out added slashes if magic_quotes_gpc is on
     if (get_magic_quotes_gpc()) {
